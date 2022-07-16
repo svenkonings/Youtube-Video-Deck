0885e187882f648a95bd57a68ea72529ed415bc1
meteoorkip <svenkonings@live.nl>
2022-07-16 21:28:44 +0200

    Improve video duration and viewcount display
    Also update dependencies

--- src/lib/VideoCard.svelte
+++ src/lib/VideoCard.svelte
@@ -6,6 +6,23 @@
 
   export let video: Video;
 
+  let duration;
+  let viewCount;
+  let publishedAt;
+
+  $: if (video) {
+    const isoDuration = Duration.fromISO(video.duration);
+    if (isoDuration.days > 0) {
+      duration = isoDuration.toFormat('d:hh:mm:ss');
+    } else if (isoDuration.hours > 0) {
+      duration = isoDuration.toFormat('h:mm:ss');
+    } else {
+      duration = isoDuration.toFormat('m:ss');
+    }
+    viewCount = abbreviate(video.viewCount);
+    publishedAt = DateTime.fromISO(video.publishedAt).toRelative({locale: 'en'});
+  }
+
   function play(): void {
     playerStore.set({videoId: video.videoId});
   }
@@ -13,12 +30,12 @@
 <a class="block w-[calc(100%-1rem)] bg-neutral-600 p-2 m-2 first:mt-0 last:mb-0 rounded-2xl" href="https://www.youtube.com/watch?v={video.videoId}" on:click|preventDefault={play}>
   <div class="inline-block relative w-[11rem] align-text-bottom p-1">
     <img src={video.thumbnailUrl} alt="" loading="lazy" width="320" height="180"/>
-    <span class="absolute right-0 bottom-0 m-1.5 px-0.5 rounded bg-black/80 text-sm">{Duration.fromISO(video.duration).toFormat('m:ss')}</span>
+    <span class="absolute right-0 bottom-0 m-1.5 px-0.5 rounded bg-black/80 text-sm">{duration}</span>
   </div>
   <div class="inline-block w-[calc(100%-11.5rem)] align-top text-sm">
     <p class="font-bold truncate" title={video.title}>{video.title}</p>
-    <p class="text-neutral-400 truncate" title="{abbreviate(video.viewCount)} views - {DateTime.fromISO(video.publishedAt).toRelative({locale: 'en'})} - {video.channelTitle}">
-      {abbreviate(video.viewCount)} views - {DateTime.fromISO(video.publishedAt).toRelative({locale: 'en'})} - {video.channelTitle}
+    <p class="text-neutral-400 truncate" title="{viewCount} views - {publishedAt} - {video.channelTitle}">
+      {viewCount} views - {publishedAt} - {video.channelTitle}
     </p>
     <p class="text-ellipsis line-clamp-3">{video.description}</p>
   </div>
--- src/util/numbers.ts
+++ src/util/numbers.ts
@@ -1,10 +1,10 @@
 const abbreviations = ['', 'K', 'M', 'B', 'T'];
 
 export function abbreviate(number: string) {
-  for (let i = abbreviations.length - 1; i >= 0; i--) {
+  for (let i = abbreviations.length - 1; i > 0; i--) {
     if (number.length > 3 * i) {
       return number.slice(0, -3 * i) + abbreviations[i];
     }
   }
-  return '0';
+  return number;
 }
