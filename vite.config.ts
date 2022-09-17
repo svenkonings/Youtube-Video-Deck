e69cf9843faa0f2d1db9ebf99ee7e0857ca41c48
meteoorkip <svenkonings@live.nl>
2022-09-17 20:53:08 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+// @ts-ignore
+import dns from 'dns'
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess';
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
